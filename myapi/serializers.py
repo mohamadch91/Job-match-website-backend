428cdace41f76164febc228ff798dbf096cd58e0
Mohamad Choupan <mohamadkhoee@yahoo.com>
2021-12-17 16:59:58 +0330

    chage

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -17,16 +17,16 @@ class ImageSerializer(FlexFieldsModelSerializer):
 
 #create serializer for Job
 class Jobserializer(serializers.ModelSerializer):
-    Image=ImageSerializer()
+   
     class Meta:
         model = Job
         fields = '__all__'
-    def create(validated_data):
-        image1=validated_data['image']
-        image2=Image.objects.create(name=image1['name'],image=image1['image'],image_ppoi=image1['image_ppoi'])
-        job = Job.objects.create(image=image2,title=validated_data['title'],description=validated_data['description'],
+    def create(self,validated_data):
+      
+        print(validated_data)
+        job = Job.objects.create(image=validated_data['image'],title=validated_data['title'],description=validated_data['description'],
                                     location=validated_data['location'],company=validated_data['company'],
-                                    link=validated_data['link'],date=validated_data['date'],ppoi=validated_data['ppoi'])
+                                    link=validated_data['link'],date=validated_data['date'],)
         return job
     def update(self, instance, validated_data):
         instance.title = validated_data.get('title', instance.title)
@@ -35,7 +35,7 @@ class Jobserializer(serializers.ModelSerializer):
         instance.company = validated_data.get('company', instance.company)
         instance.link = validated_data.get('link', instance.link)
         instance.date = validated_data.get('date', instance.date)
-        instance.ppoi = validated_data.get('ppoi', instance.ppoi)
+
         instance.save()
         return instance
 
